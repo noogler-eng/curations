c3cfebe90eb6e235d64f75f7c607310f7aec445d
noogler-eng <sharadpoddar10102003@gmail.com>
2025-06-14 23:21:14 +0530

    fixing providers 🎃

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { HeroUIProvider } from "@heroui/system";
+import HeroUiProvider from "../providers/herouiProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        <HeroUIProvider>{children}</HeroUIProvider>
+        <HeroUiProvider>{children}</HeroUiProvider>
       </body>
     </html>
   );
--- src/providers/herouiProvider.tsx
+++ src/providers/herouiProvider.tsx
@@ -5,3 +5,5 @@ import { HeroUIProvider } from "@heroui/react";
 const HeroUiProvider = ({ children }: { children: React.ReactNode }) => {
   return <HeroUIProvider>{children}</HeroUIProvider>;
 };
+
+export default HeroUiProvider;
